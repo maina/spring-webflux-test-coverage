b857a343acb3cc705a8de9a0bd8decd4a0e792b6
James <james.bugara@mastercard.com>
2020-01-18 11:23:09 +0300

    Code comments
    Signed-off-by: James <james.bugara@mastercard.com>

--- src/main/java/com/honeacademy/webclientexample/controller/KarateTestsController.java
+++ src/main/java/com/honeacademy/webclientexample/controller/KarateTestsController.java
@@ -7,12 +7,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+/**
+ * Class to handle test data and fixtures clean up
+ */
 @RestController
 @RequestMapping("/karate-tests")
 @RequiredArgsConstructor
 public class KarateTestsController {
     private final DbUtils dbUtils;
 
+    /**
+     * Clean up posts table
+     * @param request
+     * @return
+     */
     @DeleteMapping("/posts-after-scenario")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public Mono<HttpStatus> deletePost(@RequestBody final KarateTestCleanUpRequest request) {
